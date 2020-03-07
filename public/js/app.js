5ba47182ea382bc7e92d77e377860976d364326a
Avimonnu Arnob <anirudharatandash@gmail.com>
2020-03-08 01:28:03 +0600

    Added speech recognition

--- public/js/app.js
+++ public/js/app.js
@@ -5,11 +5,28 @@ const messageTwo = document.querySelector('#message-2')
 const messageThree = document.querySelector('#message-3')
 
 if (weatherForm) {
+    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+    const recognition = new SpeechRecognition()
+    recognition.interimResults = true
+
+    recognition.addEventListener('result', e => {
+        const transcript = Array.from(e.results)
+          .map(result => result[0])
+          .map(result => result.transcript)
+          .join('')
+        search.value = transcript
+    })
+
+    recognition.start()
+
+    recognition.addEventListener('end', recognition.start)
+
     weatherForm.addEventListener('click', (e) => {
         e.preventDefault()
         messageOne.textContent = 'Loading....'
         messageTwo.textContent = ''
-
+      
         const location = search.value
 
         fetch('/weather?address=' + location).then((response) => {
